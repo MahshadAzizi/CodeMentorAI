042c2467221b532c50cd6527feac75485cff232d
MahshadAzizi <mahshad.azizi98@gmail.coom>
2025-03-17 13:47:43 +0330

    feat: Add 'function' analyze schema

--- code_analysis_service/app/schema/analize.py
+++ code_analysis_service/app/schema/analize.py
@@ -1,3 +1,4 @@
+from typing import List
 from uuid import UUID
 
 from pydantic import BaseModel
@@ -10,3 +11,11 @@ class StartAnalyzeSchema(BaseModel):
 class StartAnalyzeOutputSchema(BaseModel):
     job_id: UUID
 
+
+class AnalyzeFunctionRequest(BaseModel):
+    job_id: str
+    function_name: str
+
+
+class AnalyzeFunctionResponse(BaseModel):
+    suggestions: List[str]
